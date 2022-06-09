14009518b837d8ab503598e356358b9154b28874
aayushi <aayushiofficial1995@gmail.com>
2022-06-09 18:22:27 +0530

    Added the junits for controller,converter

---
+++ src/test/java/com/mycompany/propertymanagement/service/PropertyServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.mycompany.propertymanagement.service;
+
+import com.mycompany.propertymanagement.converter.PropertyConverter;
+import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.entity.PropertyEntity;
+import com.mycompany.propertymanagement.repository.PropertyRepository;
+import com.mycompany.propertymanagement.service.impl.PropertyServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PropertyServiceImplTest {
+
+
+    @InjectMocks
+    private PropertyServiceImpl propertyServiceImpl;
+
+    @Mock
+    private PropertyConverter propertyConverter;
+    @Mock
+    private PropertyRepository propertyRepository;
+
+    @Test
+    @Disabled
+    void testSaveProperty_Succeess() {
+        PropertyDTO dto = new PropertyDTO();
+        dto.setTitle("Dummy");
+        PropertyEntity propertyEntity = new PropertyEntity();
+        propertyEntity.setTitle("Dummy");
+
+        PropertyEntity savedentity = new PropertyEntity();
+        savedentity.setTitle("Dummy");
+        savedentity.setId(1L);
+
+        PropertyDTO savedDTO = new PropertyDTO();
+        savedDTO.setTitle("Dummy");
+        savedDTO.setId(1L);
+        Mockito.when(propertyConverter.convertDTOtoEntity(Mockito.any())).thenReturn(propertyEntity);
+        Mockito.when(propertyRepository.save(Mockito.any())).thenReturn(savedentity);
+        Mockito.when(propertyConverter.convertEntityToDTO(Mockito.any())).thenReturn(savedDTO);
+        PropertyDTO result = propertyServiceImpl.saveProperty(dto);
+        Assertions.assertEquals(savedDTO.getId(), result.getId());
+
+    }
+}
